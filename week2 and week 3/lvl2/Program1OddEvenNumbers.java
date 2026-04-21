83cf6741378f064f56f9bcaacab911eec68a7a34
shazamcodes64 <shazurrahman2007@gmail.com>
2026-04-21 13:57:14 +0530

    Add Level 2 practice programs: loops, FizzBuzz, and employee bonus

---
+++ week2 and week 3/lvl2/Program1OddEvenNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Program1OddEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even number");
+                } else {
+                    System.out.println(i + " is odd number");
+                }
+            }
+        } else {
+            System.out.println("The number " + number + " is not a natural number");
+        }
+        
+        scanner.close();
+    }
+}
